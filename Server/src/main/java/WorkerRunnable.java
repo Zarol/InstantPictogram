6d8a64f091beedd9939348844726fe4625074867
Saharath Kleips <saharathkleips@nevada.unr.edu>
2015-11-15 17:21:41 -0800

    Accepts auth request, sends auth reply

--- Server/src/main/java/WorkerRunnable.java
+++ Server/src/main/java/WorkerRunnable.java
@@ -1,3 +1,7 @@
+import Messages.*;
+import Models.Database;
+
+import javax.json.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -18,20 +22,32 @@ public class WorkerRunnable implements  Runnable {
 
     public void run()
     {
-        System.out.println( "Client connected: " + clientSocket.getInetAddress() );
+        System.out.println("Client connected: " + clientSocket.getInetAddress());
         try {
             InputStream input = clientSocket.getInputStream();
             OutputStream output = clientSocket.getOutputStream();
 
+            // Get Request
             byte[] messageByte = new byte[1024];
-            int bytesRead = input.read( messageByte );
-            System.out.println( "Message Recieved: " + new String(messageByte,0,bytesRead) );
+            int bytesRead = input.read(messageByte);
+            System.out.println("Request: " + new String(messageByte,0,bytesRead) );
+
+            // Send Reply
+            JsonObject message = JsonHelper.jsonFromString(new String(messageByte, 0, bytesRead));
+            String messageType = message.keySet().iterator().next();
+            if( messageType.equals( "Authentication" ) )
+            {
+                AuthenticationMessage reply = new AuthenticationMessage( message );
+                reply.setAuthenticated(Database.getInstance().authenticateUser(reply.getUsername(), reply.getPassword()));
+                System.out.println( "Reply: " + reply.toJson().toString() );
+                output.write( reply.getMessageBytes(), 0, reply.getMessageLength() );
+            }
 
             output.close();
             input.close();
         } catch( IOException e ) {
             e.printStackTrace();
         }
-        System.out.println( "Client disconnected: " + clientSocket.getInetAddress() );
+        System.out.println("Client disconnected: " + clientSocket.getInetAddress());
     }
 }
